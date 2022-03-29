dea72191eb95142ab501782c2f85e41ad6644e2e
Lautaro Mariani Aguirre <91752333+Lautaro2210@users.noreply.github.com>
2022-03-29 08:38:07 -0300

    Ejercicio 4 Finalizado

---
+++ Assets/Scenes/Ejercicio4.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio4 : MonoBehaviour
+{
+    //Realizá un programa que permita ingresar dos números enteros num1 y num2 por Inspector.Luego, mostrar el resultado de la suma entre ambos al ejecutar la escena utilizando el siguiente formato: “La suma entre “ + num1 + “ y “ + num2 + “ da como resultado “ + num1 + num2.
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 0;
+        int num2 = 0;
+        Debug.Log("La suma entre" + num1 + "y" + num2 + "'da como resultado " + num1 + num2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
